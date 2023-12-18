1b77a1ccf9bbf5285c0673e63e4f7bd0bc40e9d4
Mars Nunez <marsnunez@Marss-MacBook-Pro.local>
2023-12-18 09:55:15 -0500

    testing

--- server/index.js
+++ server/index.js
@@ -11,9 +11,9 @@ app.use(express.json());
 app.use(taskRouter);
 
 app.get("/", async (req, res) => {
-  // const tasks = await TaskModel.find({});
-  // res.json(tasks);
-  res.send("Hello");
+  const tasks = await TaskModel.find({});
+  res.json(tasks);
+  // res.send("Hello");
 });
 
 mongoose
